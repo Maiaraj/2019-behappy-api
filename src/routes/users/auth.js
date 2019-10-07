1c5ea72027332a97f7894107f75b1370d45bf4b4
Maiaraj <jessicamayara5@live.com>
2019-10-07 11:47:26 -0300

    autenticação parte final

--- src/routes/users/auth.js
+++ src/routes/users/auth.js
@@ -6,10 +6,7 @@ export default {
   options: {
     auth: "simple"
   },
-<<<<<<< HEAD
-  handler: (request, reply) => request.auth.credentials
-=======
-  handler: (request, reply) => ({ msg: "logged" })
->>>>>>> c119b435276c4730648b838467f110312288ef7c
-};
 
+  handler: (request, reply) => request.auth.credentials
+
+};
--- src/server.js
+++ src/server.js
@@ -1,26 +1,17 @@
 import Hapi from "@hapi/hapi";
-<<<<<<< HEAD
 import { User, Token } from "./models";
-=======
-import { User } from "./models";
->>>>>>> c119b435276c4730648b838467f110312288ef7c
 
 const server = new Hapi.Server({
   port: process.env.PORT || 8000,
   debug: { request: ["*"] }
 });
 
-<<<<<<< HEAD
 const basic_validate = async (request, username, password) => {
-=======
-const validate = async (request, username, password) => {
->>>>>>> c119b435276c4730648b838467f110312288ef7c
   const user = await User.login(username, password);
 
   if (user == undefined) {
     return { credentials: null, isValid: false };
   }
-<<<<<<< HEAD
   const token = Token.add(user);
   return {
     credentials: { id: user.oid, username: user.login, token: token },
@@ -29,11 +20,13 @@ const validate = async (request, username, password) => {
 };
 
 const token_validate = async (user, request) => {
-  token = Token.findByUser(user);
+  const token = Token.findByUser(user);
   if (token == undefined) {
     return { credentials: null, isValid: false };
   } else {
-    const credentials = { id: user.id, name: user.name, token: token };
+    const credentials = { id: user.oid, name: user.name, token: token };
+    // bug fixed
+    //    const credentials = { id: user.id, name: user.name, token: token };
     return { credentials, isValid: true };
   }
 };
@@ -46,18 +39,6 @@ const init = async () => {
     validate: token_validate,
     verifyOptions: { algorithms: ["HS256"] }
   });
-=======
-  return { credentials: { id: user.oid, username: user.login }, isValid: true };
-};
-
-const init = async () => {
-  await server.register([
-    {
-      plugin: require("@hapi/basic")
-    }
-  ]);
-  server.auth.strategy("simple", "basic", { validate });
->>>>>>> c119b435276c4730648b838467f110312288ef7c
 
   await server.register({
     plugin: require("hapi-router"),
@@ -72,4 +53,3 @@ const init = async () => {
 };
 
 init();
-
